6217ea7131631e7a90af9d1af4043ca6f8b81036
lostsquirrel <songli1988@gmail.com>
2014-12-10 22:40:09 +0800

    add introduction to algorithm

--- src/chapter1/demo_1.1.3.c
+++ src/chapter1/demo_1.1.3.c
@@ -20,7 +20,7 @@ double f1(int n, double a[n], double x) {
 
 double f2(int n, double a[n], double x) {
 	int i;
-	double p = a[0] ;
+	double p = a[n] ;
 	for ( i = n; i> 0; --i)
 	{
 		p = a[i - 1] + x*p;
@@ -28,8 +28,32 @@ double f2(int n, double a[n], double x) {
 	return p;
 
 }
+
+
+//第1讲讨论：再试一个多项式
+double f3(int n, double a[n], double x) {
+	int i;
+	double p = a[0] ;
+	for (i = 0; i <= n; ++i)
+	{
+		p += (pow(x, i) / a[i]  );
+	}
+	return p;
+}
+
+double f4(int n, double a[n], double x) {
+	int i;
+	double p = 1 / a[n] ;
+	for ( i = n; i> 0; --i)
+	{
+		p = 1/a[i - 1] + x*p;
+	}
+	return p;
+
+}
+
 /*
-f 选择执行的函数 1 f1, 2, f2
+f 选择执行的函数 1 f1; 2, f2; 3, f3; 4, f4
 max_repeat 重复执行函数的次数
 */
 void tester(int f, int max_repeat) {
@@ -43,20 +67,38 @@ void tester(int f, int max_repeat) {
 	for (i=0; i < MAXN; i++) {
 		a[i] = i * 1.0;
 	}
-	start = clock();
 	//code count on time
 	if (f == 1) {
+		start = clock();
 
 		for (i=0; i < max_repeat; i++) {
 			f1(MAXN -1, a, 1.1);
 		}
+		stop = clock();
+
 	} else if (f == 2) {
+		start = clock();
 		for (i=0; i < max_repeat; i++) {
 			f2(MAXN -1, a, 1.1);
 		}
-	}
-	stop = clock();
+		stop = clock();
 
+	} else if (f == 3) {
+		start = clock();
+		for (i=0; i < max_repeat; i++) {
+			f3(MAXN -1, a, 1.1);
+		}
+		stop = clock();
+
+	} else if (f == 4) {
+		start = clock();
+		for (i=0; i < max_repeat; i++) {
+			f4(MAXN -1, a, 1.1);
+		}
+		stop = clock();
+
+	}
+	
 	duration = ((double)(stop - start)) / CLOCKS_PER_SEC;
 
 	printf("durate for f%d is %6.2es\n", f, duration / max_repeat);
@@ -66,6 +108,8 @@ int main(void) {
 	int max_repeat = 1e6;
 	tester(1, max_repeat);
 	tester(2, max_repeat);
+	tester(3, max_repeat);
+	tester(4, max_repeat);
 	printf("CLK_TCK=%lu\n", CLOCKS_PER_SEC);
 
 	return 0;
