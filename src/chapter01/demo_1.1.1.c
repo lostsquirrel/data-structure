c34b49a5c86fccc777beac8d332b80d7224956f0
lostsquirrel <songli1988@gmail.com>
2015-04-13 22:08:42 +0800

    change the iterate implements print number

--- src/chapter01/demo_1.1.1.c
+++ src/chapter01/demo_1.1.1.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /*
+ 1.1.2 关于空间使用 - 例：PrintN函数实现（04:08）
+ 输入一个正整数N，打印1到这个数之间的所有整数
 
-
-1.1.2 关于空间使用 - 例：PrintN函数实现（04:08）
-输入一个正整数N，打印1到这个数之间的所有整数
-
-*/
+ */
 
 void printN(int n) {
-	while(n) {
-		printf("%d\n", n);
-		n--;
+	int i = 1;
+	while (i <= n) {
+		printf("%d\n", i);
+		i++;
 	}
 }
 
 void printM(int m) {
 	if (m) {
-		printM(m  - 1);
+		printM(m - 1);
 		printf("%d\n", m);
 	}
 }
@@ -25,13 +24,13 @@ void printM(int m) {
 int main(void) {
 	int x;
 	scanf("%d", &x);
-	// printN(x);
-	//printN输入1 000 000 跑了好一会
-
-	//===============
 	printf("input number: %d\n", x);
-	printM(x);
+	//printN输入1 000 000 跑了好一会
+	printN(x);
+
+	//===============递归实现
 	//printM输入1 000 000 能跑（8G RAM）, 10 000 000 就不行了 Segmentation fault (core dumped)
+//	printM(x);
 
 	return 0;
 }
